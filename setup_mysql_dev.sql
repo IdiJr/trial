68a86a44f84c8ac4cc9fbc99d635b8789adb67ea
IdiJr <idialijnr@gmail.com>
2023-08-22 11:10:13 +0000

    setup for setup_mysql_dev

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,7 @@
+-- setup for MYSQL server
+-- creates the db hbnb_dev_db, and grant prvilleges
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
